e07aa0e71a510371c8f58d3ef7779967544f5acf
Moti Gabay <motigabay18@gmail.com>
2023-06-13 16:33:12 +0300

    ok

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const router = express.Router();
+const {guides} = require("../json/guides")
 
 router.get("/", async(req,res) => {
-  res.json({msg:"Express homepage work"});
+  res.json(guides);
 })
 
 module.exports = router;
